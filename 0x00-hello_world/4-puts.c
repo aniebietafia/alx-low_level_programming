e6c35a8caa1e032f04546db6ec327621ae695817
AfiaAniebiet <leeboy4u2002@yahoo.com>
2022-06-30 09:20:34 -0700

    Task 4 completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,9 @@
-#!/bin/bash
-#include<stdio.h>
-
+#include <stdio.h>
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
