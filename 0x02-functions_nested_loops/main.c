b84def7df6bb754ff1023bde12046ca3470365de
AfiaAniebiet <leeboy4u2002@yahoo.com>
2022-07-05 06:26:13 -0700

    Corrections to the tasks.

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Checks the code for the C programming.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_akphabet();
+	print_alphabet_x10();
+	return (0);
+}
