d180dc25f61652944054925ec2ea2c6c3861aa4c
AfiaAniebiet <leeboy4u2002@yahoo.com>
2022-07-07 06:03:52 -0700

    Project 0x04 in C - More Functions

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,13 +8,17 @@
 
 int main(void)
 {
-	unsigned long int i = 3, n = 612852475143;
+	long int n, y;
 
-	for (; i < 12057; i += 2)
+	n = 612852475143;
+	for (y = 2; y <= n; y++)
 	{
-		while (n % i == 0 && n != i)
-			n /= i;
+		if (n % y == 0)
+		{
+			n /= y;
+			y--;
+		}
 	}
-	printf("%lu\n", n);
+	printf("%ld\n", y);
 	return (0);
 }
