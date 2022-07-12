914ca9e89c1c6d2a28507e03a9de8a90cb65191d
AfiaAniebiet <leeboy4u2002@yahoo.com>
2022-07-12 07:57:27 -0700

    Pointers, Arrays and Strings Project

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - Takes a pointer to a parameter and updates its value.
  * @n: input integer.
+ *
  * Return: no return.
  */
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@ void swap_int(int *a, int *b);
 void int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(chr *s);
+void rev_string(chr *s)
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
