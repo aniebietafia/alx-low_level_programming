b55de84c0d90475274d4151fa11cd774628cc006
AfiaAniebiet <leeboy4u2002@yahoo.com>
2022-07-12 09:16:46 -0700

    Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,10 +3,10 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-void int _strlen(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(chr *s)
+void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
