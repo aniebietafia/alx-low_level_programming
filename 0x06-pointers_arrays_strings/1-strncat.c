55e2fe6a2b0440f963acfea8c8defdfb77221787
AfiaAniebiet <leeboy4u2002@yahoo.com>
2022-07-14 04:44:07 -0700

    More Pointers, Arrays and Strings Project

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings,
+ * @dest: Destination.
+ * @src: Source.
+ * @n: Amount of bytes used from src.
+ *
+ * Return: The pointer to dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,8 @@
+Task 0: A function that concatenates two strings.
+Task 1: A function that concatenates two strings.
+Task 2: A function that copies a string.
+Task 3: A  function that compares two strings.
+Task 4: A function that reverses the content of an array of integers.
+Task 5: A function that changes all lowercase letters of a string to uppercase.
+Task 6: A function that capitalizes all words of a string.
+Task 7: A function that encodes a string into 1337.
