a7e31701d14018c8defba628a00130d582ecc78b
AfiaAniebiet <leeboy4u2002@yahoo.com>
2022-07-15 11:13:58 -0700

    More Pointers, Arrays and Strings Project

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,50 +9,25 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	while (s[i])
+	while (*s(s + i))
 	{
-		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-			s[i] -= 32;
-		if (check_separators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-			s[i + 1] -= 32;
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
 		i++;
 	}
-
 	return (s);
-}
-
-/**
- *
- *check_seperators - Separators of words: space, tabulation, new line,
- *
- *,, ;, ., !, ?, ", (, ), {, and }
- * @c: an input character
- *
- * Return: 1 if seperator, 0 otherwise
- */
-
-int check_seperators(char c)
-
-{
-
-		int i = 0;
-
-			char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
-
-						'"', '(', ')', '{', '}' };
-
-				for (; i < 13; i++)
-
-						{
-
-									if (c == seperators[i])
-
-													return (1);
-
-										}
-
-					return (0);
-
 }
