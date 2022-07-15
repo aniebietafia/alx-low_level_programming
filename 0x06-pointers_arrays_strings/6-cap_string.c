1b22de60da2f30a8484220bfba18c667042a4931
AfiaAniebiet <leeboy4u2002@yahoo.com>
2022-07-15 00:44:21 -0700

    More Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,25 +9,50 @@
 
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i = 0;
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	while (s[i])
 	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122;
-							*(s + (count + 1)) = *(s + (count + 1)) - 32;
-							break;
-							}
-							}
-							count++;
-							}
-							return (s);
-							}
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
+		if (check_separators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
+		i++;
+	}
 
+	return (s);
+}
+
+/**
+ *
+ *check_seperators - Separators of words: space, tabulation, new line,
+ *
+ *,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ *
+ * Return: 1 if seperator, 0 otherwise
+ */
+
+int check_seperators(char c)
+
+{
+
+		int i = 0;
+
+			char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+
+						'"', '(', ')', '{', '}' };
+
+				for (; i < 13; i++)
+
+						{
+
+									if (c == seperators[i])
+
+													return (1);
+
+										}
+
+					return (0);
+
+}
