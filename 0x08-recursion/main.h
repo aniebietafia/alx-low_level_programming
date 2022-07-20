d829541d26d068b0c2d9ec9b753fdfa231d8a32c
AfiaAniebiet <leeboy4u2002@yahoo.com>
2022-07-20 09:17:01 -0700

    Recursion Projects

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_divisible(int num, int div);
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
 
 #endif
